daf6a9eeda85505776d40e0121f1b3aef27bd1c2
Moonquit <exzufaced@gmail.com>
2021-01-21 22:34:38 +0300

    manual for compiling to exe

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+"""
+Vk reader exe - https://github.com/Moonquit/vkdreader/releases/download/1.0/vkdreader.exe
+This directory contains the files used for compiling to exe. You can compile it yourself using pyinstaller:
+
+> cd vkdreader/app
+> pip install pyinstaller
+> pyinstaller --onefile --icon=icon.ico main.py
+
+The compiled exe will be located in dist/
+"""
